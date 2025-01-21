3c770ce151c72931d0440b81ec756145b941dfbc
Karima002 <karima.mouadi@hva.nl>
2025-01-21 21:46:08 +0100

    interactie: add to list bij klik & sluit chat button interactie

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,10 @@
-// Chatbutton
-//Stap 1: selecteren geschikt element
+// **Chatbutton**//
+//Selecteren geschikt element
 const openChatButton = document.querySelector ('.chat-button')
 const theSearch = document.querySelector ('.zoeken')
+const sluitChat = document.querySelector ('.close-chat')
 
-//Stap 2: voeg event en functie toe
+//Voeg event en functie toe
 openChatButton.addEventListener('click', openSearch)
 
 
@@ -11,6 +12,27 @@ function openSearch() {
     theSearch.classList.add('toonSearch')
 }
 
-//Stap 3: feedback aan de gebruiker --> CSS
+//**Sluit chat**//
+sluitChat.addEventListener ('click', sluitChatMenu)
+
+function sluitChatMenu() {
+    theSearch.classList.remove('toonsearch') //in de functie verwijder de class van chat
+}
+
+ 
+//**add to wishlist**//
+const addList = document.querySelector ('.save-button')
+const popUpList = document.querySelector ('.pop-up')
+const iconState = true; 
+
+addList.addEventListener ('click', toonPopUp)
+
+function toonPopUp() {
+    popUpList.classList.add('show'); 
+}
+
+setTimeout(function() {
+    popUpList.classList.remove('show'); //zorgt ervoor dat de class 'show' automatich verdwijnt na 5 sec (5000 milliseconden)
+}, 5000);
 
 
